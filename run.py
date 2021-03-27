62f69879093c9c67d118ef52b200c6c9caeb04a5
bernardo300 <bernardolopes.luz@hotmail.com>
2021-03-27 20:29:57 -0300

    adicao rota /soma com post

--- run.py
+++ run.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Flask na dio.'
+    return jsonify({'nome': 'Kennedy'})
+
+
+@app.route('/soma/<int:a>/<int:b>', methods=['GET'])
+def soma(a, b):
+    total = a + b
+    return jsonify({'total': str(total)})
+
+
+@app.route('/soma', methods=['POST', 'GET'])
+def soma2():
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        soma = sum(data['dados'])
+        return jsonify({'total': soma})
+    elif request.method == 'GET':
+        return jsonify({'total': 45})
 
 
 if __name__ == '__main__':
